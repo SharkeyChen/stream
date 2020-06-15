32bf131d7f00daf0e40b355c7cfdccccd6f98d58
sharkey <1538720091@qq.com>
2020-06-15 10:52:55 +0800

    显示服务器公网IP

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,10 +1,12 @@
 import util.MyLogger;
+import util.Tool;
 
 import java.io.IOException;
 
 public class ServerTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         MyServer s = new MyServer();
+        MyLogger.log("公网IP：" + Tool.getNetIp());
         MyLogger.log("端口号：" + s.getPort());
         MyLogger.log("Address：" + s.getAddress());
         s.start();
